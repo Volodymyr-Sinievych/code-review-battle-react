4cbabd720d3710bd4a78ec02e7cd1a21c406591f
volodymyrsinievych <volodymyr.sinievych@socialtechnologies.io>
2026-09-09 11:25:08 +0300

    ci: deploy demo to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/code-review-battle-react/',
   plugins: [react()],
   test: {
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
